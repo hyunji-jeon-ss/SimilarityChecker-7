b88ab2a3c2cec1149ad041c45c2e71fdc1efa7a0
hyunji-jeon-ss <hj_ash@naver.com>
2026-07-01 15:29:06 +0900

    [feature] 1

--- SimilarityChecker/SimilarityChecker.cpp
+++ SimilarityChecker/SimilarityChecker.cpp
@@ -3,5 +3,11 @@ using std::string;
 
 class SimilarityChecker {
 public:
-
+	int runChecker(const string& str1, const string& str2) {
+		int len1 = str1.length();
+		int len2 = str2.length();
+		if (len1 == len2)
+			return 60;
+		else return 0;
+	}
 };
--- SimilarityChecker/SimilarityChecker_Test.cpp
+++ SimilarityChecker/SimilarityChecker_Test.cpp
@@ -2,6 +2,17 @@
 #include "gmock/gmock.h"
 using namespace testing;
 
+TEST(SimilarityCheckerTest, Test1)
+{
+	SimilarityChecker checker;
+	int result = checker.runChecker("ASD", "DSA");
+	int expected = 60;
+
+	EXPECT_EQ(expected, result);
+}
+
+
+
 int main() {
 	InitGoogleMock();
 	return RUN_ALL_TESTS();
