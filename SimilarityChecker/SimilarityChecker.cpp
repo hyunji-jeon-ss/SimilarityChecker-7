03dcd17d781bc898de6e11e8c205d57293607fcd
hyunji-jeon-ss <hj_ash@naver.com>
2026-07-01 16:13:08 +0900

    modified

--- SimilarityChecker/SimilarityChecker.cpp
+++ SimilarityChecker/SimilarityChecker.cpp
@@ -1,15 +1,18 @@
 #include <string>
 using std::string;
 
+#define MAX_LENGTH_SCORE 60
+#define MIN_LENGTH_SCORE 0
+
 class SimilarityChecker {
 public:
 	int runChecker(const string& str1, const string& str2) {
 		longer = std::max(str1.length(), str2.length());
 		shorter = std::min(str1.length(), str2.length());
 		if (isEqualStringLength())
-			return 60;
+			return MAX_LENGTH_SCORE;
 		if (isDouble())
-			return 0;
+			return MIN_LENGTH_SCORE;
 		return getScore();
 	}
 	double getScore()
