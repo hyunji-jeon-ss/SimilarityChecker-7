379fc536ccf6d886a03f4b659b01887958e8bd1a
hyunji-jeon-ss <hj_ash@naver.com>
2026-07-01 15:39:18 +0900

    [feature] 2

--- SimilarityChecker/SimilarityChecker.cpp
+++ SimilarityChecker/SimilarityChecker.cpp
@@ -6,8 +6,14 @@ public:
 	int runChecker(const string& str1, const string& str2) {
 		int len1 = str1.length();
 		int len2 = str2.length();
+		int longer = std::max(len1, len2);
+		int shorter = std::min(len1, len2);	
 		if (len1 == len2)
 			return 60;
-		else return 0;
+		else
+			if (longer - shorter == shorter)
+				return 0;
+			else
+				return (1 - static_cast<double>(longer - shorter) / shorter) * 60;
 	}
 };
--- SimilarityChecker/SimilarityChecker_Test.cpp
+++ SimilarityChecker/SimilarityChecker_Test.cpp
@@ -11,6 +11,24 @@ TEST(SimilarityCheckerTest, Test1)
 	EXPECT_EQ(expected, result);
 }
 
+TEST(SimilarityCheckerTest, Test2)
+{
+	SimilarityChecker checker;
+	int result = checker.runChecker("A", "BB");
+	int expected = 00;
+
+	EXPECT_EQ(expected, result);
+}
+
+TEST(SimilarityCheckerTest, Test3)
+{
+	SimilarityChecker checker;
+	int result = checker.runChecker("AAABB", "BAA");
+	int expected = 20;
+
+	EXPECT_EQ(expected, result);
+}
+
 
 
 int main() {
